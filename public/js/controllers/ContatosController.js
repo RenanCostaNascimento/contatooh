7eccafcd99a007a6cdb83a9f06c6e016285aa345
Renan Nascimento <renan.nascimento@NBVIX1240.ISH.LOCAL>
2015-10-14 16:27:26 -0300

    Capítulo 4 concluído - AngularJs - Bootstrap

---
+++ public/js/controllers/ContatosController.js
@@ -0,0 +1,27 @@
+angular.module('contatooh' ).controller('ContatosController' ,
+function($scope) {
+
+  $scope.contatos = [
+    {
+      "_id": 1,
+      "nome": "Contato Angular 1",
+      "email": "cont1@empresa.com.br"
+    },
+    {
+      "_id": 2,
+      "nome": "Contato Angular 2",
+      "email": "cont2@empresa.com.br"
+    },
+    {
+      "_id": 3,
+      "nome": "Contato Angular 3",
+      "email": "cont3@empresa.com.br"
+    }
+  ];
+  $scope.total = 0;
+  $scope.filtro = '';
+
+  $scope.incrementa = function() {
+    $scope.total++;
+  };
+});
