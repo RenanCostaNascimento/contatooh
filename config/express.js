1b27033bffbf8e5480f1ae8a55041ef4103c4866
Renan Nascimento <renan.nascimento@NBVIX1240.ISH.LOCAL>
2015-10-19 11:57:14 -0200

    Capítulo 8 - Autenticação com Passport - controle de acesso implementado: login, logout, permissões.

--- config/express.js
+++ config/express.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser' );
+var cookieParser = require('cookie-parser' );
+var session = require('express-session' );
+var passport = require('passport' );
 
 module.exports = function() {
   var app = express();
@@ -15,12 +18,20 @@ module.exports = function() {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(require('method-override' )());
+  app.use(cookieParser());
+  app.use(session(
+    { secret: 'homem avestruz' ,
+    resave: true,
+    saveUninitialized: true
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   // definir rotas deve ser a última coisa a ser feita
   load('models' , {cwd: 'app' })
-    .then('controllers' )
-    .then('routes' )
-    .into(app);
+  .then('controllers' )
+  .then('routes' )
+  .into(app);
 
   return app;
 };
